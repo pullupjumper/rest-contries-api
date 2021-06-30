c195b909378117a4a5b420ad135f32b82ab3034b
Chris <kendiablo0@gmail.com>
2021-06-30 14:19:08 +0800

    Added dark theme.

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,22 @@
 import '../scss/index.scss';
 
 const dropdown = document.querySelector('.dropdown');
+const toggleBtn = document.querySelector('.toggle-btn');
 
 
 initComponents();
 
 
 function initComponents() {
+  toggleBtn.addEventListener('click', () => {
+    if (document.documentElement.hasAttribute('theme')) {
+      document.documentElement.removeAttribute('theme');
+    }
+    else {
+      document.documentElement.setAttribute('theme', 'dark');
+    }
+  });
+
   dropdown.addEventListener('click', () => {
     dropdown.classList.toggle('dropdown-show');
   });
