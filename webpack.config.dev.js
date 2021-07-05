d91530ceb3b63dbbbb181d3f52f9227d96277171
Chris <kendiablo0@gmail.com>
2021-07-05 16:00:10 +0800

    Added detail.html

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,40 +3,49 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/js/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|gif|eot|ttf|woff)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
+  mode: 'development',
+  entry: {index: './src/js/index.js', detail: './src/js/detail.js',},
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-            template: './src/pages/index.html'
-        }),
-        new webpack.HotModuleReplacementPlugin()
+      },
+      {
+        test: /\.(png|svg|jpg|gif|eot|ttf|woff)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/pages/index.html',
+      filename:'index.html',
+      chunks: ['index']
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/pages/detail.html',
+      filename:'detail.html',
+
+      chunks: ['detail']
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
 };
