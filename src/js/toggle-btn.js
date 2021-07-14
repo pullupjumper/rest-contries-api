2c5f10948f23cae0caf795b2f4afbcb202fa43fc
Chris <kendiablo0@gmail.com>
2021-07-14 21:37:39 +0800

    Updated toggle-btn.js

--- src/js/toggle-btn.js
+++ src/js/toggle-btn.js
@@ -1,13 +1,31 @@
+function toggleHandler() {
+  if (document.documentElement.hasAttribute('theme')) {
+    document.documentElement.removeAttribute('theme');
+    window.localStorage.setItem('theme', 'default');
+  }
+  else {
+    document.documentElement.setAttribute('theme', 'dark');
+    window.localStorage.setItem('theme', 'dark');
+  }
+}
+
+
 export default function initToggleBtn() {
   const toggleBtn = document.querySelector('.toggle-btn');
+  let storageData = window.localStorage.getItem('theme');
 
-  toggleBtn.addEventListener('click', () => {
-    if (document.documentElement.hasAttribute('theme')) {
-      document.documentElement.removeAttribute('theme');
-    }
-    else {
-      document.documentElement.setAttribute('theme', 'dark');
-    }
-  });
+  if (storageData == null) {
+    storageData = "default";
+    window.localStorage.setItem('theme', 'default');
+  }
 
+  if (storageData === 'default' && document.documentElement.hasAttribute('theme')) {
+    document.documentElement.removeAttribute('theme');
+  }
+
+  if (storageData === 'dark') {
+    document.documentElement.setAttribute('theme', 'dark');
+  }
+
+  toggleBtn.addEventListener('click', toggleHandler);
 }
